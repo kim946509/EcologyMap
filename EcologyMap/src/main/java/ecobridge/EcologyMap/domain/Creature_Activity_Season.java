3f5f8fa874f45e93d5ba2359b5709847731c13c2
김의찬 <128672362+euichan0927@users.noreply.github.com>
2023-08-09 01:13:24 +0900

    test commit

--- EcologyMap/src/main/java/ecobridge/EcologyMap/domain/Creature_Activity_Season.java
+++ EcologyMap/src/main/java/ecobridge/EcologyMap/domain/Creature_Activity_Season.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
