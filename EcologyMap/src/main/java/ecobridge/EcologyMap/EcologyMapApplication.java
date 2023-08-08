36f19ccafffab9677c22828a5152a37c69eed365
kim946509 <kim946509@gmail.com>
2023-08-08 17:17:47 +0900

    sql connection

--- EcologyMap/src/main/java/ecobridge/EcologyMap/EcologyMapApplication.java
+++ EcologyMap/src/main/java/ecobridge/EcologyMap/EcologyMapApplication.java
@@ -2,8 +2,10 @@ package ecobridge.EcologyMap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"ecobridge.EcologyMap.domain"})
 public class EcologyMapApplication {
 
 	public static void main(String[] args) {
